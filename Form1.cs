428a399478a6d677eb027a01d3a680e0d6ca2c0a
jocaqes <pancho_jose_carlos@hotmail.com>
2016-12-06 18:19:30 -0600

    Funcion MCD

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HT1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private int MCD(int m, int n)//ejemplo de mcd no es de mi autoria
+        {
+            if (n > m) return MCD(n, m);
+            else if (n == 0) return m;
+            else return MCD(m - n, n);
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int m = int.Parse(textBox1.Text);
+                int n = int.Parse(textBox2.Text);
+                textBox3.Text = MCD(m, n).ToString();
+            }
+            catch
+            {
+                textBox3.Text = "Error, valor invalido";
+            }
+
+        }
+    }
+}
